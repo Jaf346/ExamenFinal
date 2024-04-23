b876a5a042311ecb6c4b35785d00a1ac4b609019
Jafet <Jafet@DESKTOP-GBOGUTK>
2024-04-22 22:51:27 -0600

    Agregar archivos de proyecto.

---
+++ ExamenFinal/Capa Presentacion/Clientes_View.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExamenFinal.Capa_Presentacion
+{
+    public partial class Clientes_View : System.Web.UI.Page
+    {
+        public class Cliente
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Email { get; set; }
+            
+        }
+
+        public class Agente
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Email { get; set; }
+           
+        }
+    }
+}
---
+++ ExamenFinal/Capa Presentacion/Login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExamenFinal.Presentacion
+{
+    public partial class Login : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Button1.Click += new EventHandler(Button1_Click);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = username.Value;
+            string password = password.Value;
+
+            if (username == "usuario" && password == "contraseña")
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Inicio de sesión exitoso')", true);
+            }
+            else
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Nombre de usuario o contraseña incorrectos')", true);
+            }
+        }
+    }
+}
